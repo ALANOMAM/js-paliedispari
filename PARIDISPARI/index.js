c0eb124ddd333ccad80cddb3d927048cb1357437
alan_omam <omamalan@hotmail.it>
2024-02-06 15:26:54 +0100

    creato la funzione che genera nu eri random per il pc

---
+++ PARIDISPARI/index.js
@@ -0,0 +1,17 @@
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari.
+Inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Se l'utente ha indovinato l'esito (pari o dispari) della somma ha vinto, altrimenti ha perso.
+*/
+
+
+function PcRandom(){
+ let random = Math.floor((Math.random()*5)+1)
+ return random
+}
+
+console.log(PcRandom())
